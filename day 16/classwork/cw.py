47b9c88617c6a65e9a889aa3bfdc3ad52bcb616c
ioane22 <boghrashviliioane@gmail.com>
2024-11-27 22:17:49 +0400

    second commit

---
+++ day 16/classwork/cw.py
@@ -0,0 +1,15 @@
+print(True or False)
+print(False and True)
+print(True or False)
+print(True and False)
+print(False or False)
+print(True and False)
+print(True or True)
+print(True and False)
+print(False or True)
+print(True and False)
+
+
+
+num = int(input("enter your number"))
+print(num>10 or False)
---
+++ day 16/homework/cw.py
@@ -0,0 +1,27 @@
+print(True and True or False)
+print(False or True or True)
+print(True or True or False)
+print(True and False or False)
+print(True or False or False)
+print(False or False or True)
+print(False and True or True)
+print(True and True or False)
+print(True and True or False)
+print(True or False or True)
+
+
+num1 = int(input("enter your number"))
+print(num1 > 10 or False)
+print(num1 > 15 and True)
+print(num1 > 17 or False)
+print(num1 > 19 and True)
+print(num1 > 21 and True)
+print(num1 > 56 and False)
+print(num1 > 78 or True)
+print(num1 > 19 or False)
+print(num1 > 12 and False)
+
+
+
+
+
