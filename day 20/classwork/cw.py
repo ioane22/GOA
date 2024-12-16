46136cacaa1f41b862e6dd9bc046571226978e3c
ioane22 <boghrashviliioane@gmail.com>
2024-12-16 19:30:04 +0400

    second commit

---
+++ day 20/classwork/cw.py
@@ -0,0 +1,14 @@
+for i in range(10):
+    print("ioane boghrashvili")
+
+for i in range(2, 21 , 2): 
+   print(i)
+
+for i in range(11,40,2):
+    print(i)
+
+numbers_sum = 0
+for i in range(10,31,2):
+    numbers_sum = numbers_sum + i
+print(numbers_sum)
+    
