9e5d8528522a55000bee4a98c1b685bdf1c8b200
ioane22 <boghrashviliioane@gmail.com>
2025-01-19 13:45:26 +0400

    second commit

---
+++ day 28/classwork/cw.py
@@ -0,0 +1,20 @@
+
+
+#              0       1        2            3            4
+classwork = ['bmw', 'audi', 'mersedes', 'lamborgin', 'hyundai']
+# დავბეჭდოთ სიის ინდექსები
+print(classwork[0])
+print(classwork[1])
+print(classwork[2])
+print(classwork[3])
+print(classwork[4])
+
+classwork2 = 23,45,765
+classwork2[0]=(classwork2[0]+10)
+print(classwork2[0])
+print(classwork2[0]+classwork2[2])
+
+
+user_number = int(input("enter index:"))
+classwork3 = ["m5 f90", "m4", "m3", "m2","m5 e60"]
+print(classwork3[user_number])
---
+++ day 28/homework/cw.py
@@ -0,0 +1,60 @@
+# list ნიშნავს ცხრილს და როდესაც ბევრი ინფორმაციის შსნახვა გვინდა ვიყენებთ list-ს
+# index არის ელემენტის პოზიცია
+# mutable არის შეცვლადი და ცვალებადი
+# immutable არის არ არის შცვლადი
+
+
+#                0     1     2      3    4
+numbers_list = [1, 67, 786, 24, 4]
+print(numbers_list[1])
+print(numbers_list[3])
+print(numbers_list[4])
+
+
+#             0         1         2        3         4      5  6  7  8 9     10    11     12    13    14    15     16    17     18   19  20
+all_list = ["bmw", "mersedes", "audi", "porche", "ferrari", 1, 2, 3, 4, 5, True, False, 23.5, 67.5, 60.4, 567.7, 56.8, False, True, 9.9, 9]
+print(all_list[0])
+print(all_list[1])
+print(all_list[2])
+print(all_list[3])
+print(all_list[4])
+print(all_list[5])
+print(all_list[6])
+print(all_list[7])
+print(all_list[8])
+print(all_list[9])
+print(all_list[10])
+print(all_list[11])
+print(all_list[12])
+print(all_list[13])
+print(all_list[14])
+print(all_list[15])
+print(all_list[16])
+print(all_list[17])
+print(all_list[18])
+print(all_list[19])
+print(all_list[20])
+
+
+
+number = [1, 2, 3, 4, 5,]
+print(number[-5])
+print(number[-3])
+print(number[-1])
+
+
+
+name = input("enter your name")
+print(name[0])
+print(name[-1])
+
+
+
+
+
+
+
+
+
+
+
