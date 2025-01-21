cc6ebd59c343e68a095fdfff2e3b18d9fd49628e
ioane22 <boghrashviliioane@gmail.com>
2025-01-21 17:19:49 +0400

    second commit

---
+++ day 30/classwork/cw.py
@@ -0,0 +1,37 @@
+
+
+
+
+level30 = ["audi", "tesla", 23.6, 8.65, 12, 9, True, False, [1, 2, 3], ["ა", "ბ", "გ"]]
+print(level30[2:8])
+print(level30[0:10:4])
+
+
+
+classwork2=["bmw", "mersedes", "audi", "barcelona", "real madrid", 1, 4, 5, 8, 8, 7.5, 789.5, 87.9, 67.8, 690.77, 7997.89]
+print(classwork2[::1])
+print(classwork2[::-1])
+print(classwork2[1:4])
+print(classwork2[3:7])
+print(classwork2[2:9])
+print(classwork2[1:7])
+print(classwork2[5:8])
+print(classwork2[1:4:2])
+print(classwork2[3:7:2])
+print(classwork2[2:9:2])
+print(classwork2[1:7:2])
+print(classwork2[5:8:2])
+print(classwork2[::5])
+print(classwork2[::8])
+print(classwork2[::9])
+print(classwork2[::2])
+print(classwork2[::4])
+
+
+
+classwork3 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+print(classwork3[-1:])
+print(classwork3[:-2])
+print(classwork3[:-3])
+print(classwork3[-4:])
+print(classwork3[-5:])
---
+++ day 30/homework/cw.py
@@ -0,0 +1,59 @@
+
+# მე-2 დავალება
+#               0         1            2           3           4
+barcelona_hw2 = ["yamal", "raphinia", "lewandoski", "pedri", "verandroez"]
+print(barcelona_hw2[0:5])
+
+# მე-3 დავალება
+#                  0         1            2           3           4
+real_madrid_hw3 = ["benzema", "modric", "vini.jr", "bellingem", "courtis"]
+print(real_madrid_hw3[2:5])
+
+# მე-4 დავალება
+#            0  1  2  3  4  5  6  7  8   9
+homework4 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+print(homework4[0:5:2])
+
+# მე-5 დავალება
+#            0   1    2    3  4    5   6    7   8    9
+homework5 = [23, 54, 765, 65, 75, 76, 876, 876, 76, 987]
+print(homework5[0:10:3])
+
+# მე-6 დავალება
+#               0      1      2      3      4 
+homework6 = [654.76, 76.76, 76.76, 345.65, 7.8]
+print(homework6[-1:])
+
+# მე-7 დავალება
+#               0          1         2     3  4   5   6   7   8   9
+homework7 = ["winter", "spring", "summer", 1, 56, 68, 70, 90, 74, 50]
+print(homework7[1:9])
+
+# მე-8 დავალება
+#              0        1         2        3            4          5           6          7         8           9
+homework8 = ["bmw", "mersedes", "audi", "hyudai", "mitsubishi", "tesla", "tesla track","porche", "ferrari", "lamborgin"]
+print(homework8[4:11])
+
+
+
+
+# მე-10 დავალება
+#                 0           1        2        3         4         5         6         7            8             9
+homework11 = ["goa best", "tomato", "cars", "numbers", "float", "intenger", "src", "barcelona", "real madrid", "brazil"]
+print(homework11[0:5])
+
+# მე-11 დავალება
+#                   0           1        2       3       4      5       6        7       8      
+homevork12 = ["goa is best", "tomato", "arm", "neck", "leg", "head", "apple", "book", "bear"]
+print(homevork12[3:])
+
+# მე-12 დავალება
+#                      0       1       2         3         4           5                6          7         8         9
+homework13_cars = ["honda", "bmw", "mersedes", "audi", "hyundai", "lamborgin", "lamborgin urus", "opel", "suzuki", "ferrari"]
+print(homework13_cars[0:5])
+print(homework13_cars[-5:])
+
+# მე-13 დავალება
+#                     0         1            2           3           4           5         6         7           8           9
+homework14_all = ["yamal", "raphinia", "lewandoski", "pedri", "verandroez", "benzema", "modric", "vini.jr", "bellingem", "courtis"]
+print(homework14_all[5:10])
